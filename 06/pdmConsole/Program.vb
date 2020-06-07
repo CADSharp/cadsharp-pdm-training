a85f4b5e3a9353b6caf6764b404fb5e48cca018e
jliliamen <amen@bluebyte.biz>
2020-06-07 16:13:00 -0700

    06

--- 06/pdmConsole/Program.vb
+++ 06/pdmConsole/Program.vb
@@ -44,78 +44,69 @@ Module Program
         End If
 
 
-        Dim folder As IEdmFolder5
-        Dim filePath As String = "C:\PDM2020\Example\Axle_&.sldprt"
-        Dim axlePart = vault.GetFileFromPath(filePath, folder)
+        Dim rootFolder As IEdmFolder5
+        rootFolder = vault.RootFolder
+        Dim exampleSubFolder As IEdmFolder5
 
-        Dim handle As Int32
+        exampleSubFolder = rootFolder.GetSubFolder("Example")
+
+        Dim filePath As String
+        filePath = "C:\PDM2020\Example\axle_&.sldprt"
+
+        Dim folder As IEdmFolder5
+        Dim axlePart As IEdmFile5
+
+        axlePart = vault.GetFileFromPath(filePath, folder)
+
+        Dim handle As Integer
         handle = System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle.ToInt32()
 
-        ' explanation:
-        ' Locked = False -> file Is checked In 
-        ' Locked = True  -> file Is checked out
+        'explanation: 
+        'Locked = False =>  file is checked in 
+        'Locked = True => file is checked out 
 
         If (axlePart.IsLocked = False) Then
 
-            'check file out 
-            Console.WriteLine($"Checking out {axlePart.Name}...")
+            Console.WriteLine($"Checking {axlePart.Name} out...")
             axlePart.LockFile(folder.ID, handle, 0)
             Console.WriteLine($"Checked out {axlePart.Name} = {axlePart.IsLocked}")
-
 
             Console.WriteLine("Sleeping for several seconds...")
             System.Threading.Thread.Sleep(3000)
 
             Dim ret As String = String.Empty
-
             While (ret <> "y" And ret <> "n")
 
-                Console.WriteLine("Would you like to undo check out? [y] | [n]")
+                Console.WriteLine("Would you like to undo the check out ? [y] | [n]")
                 ret = Console.ReadLine()
 
             End While
 
             If (ret.Trim().ToLower() = "y") Then
-                'undo check out 
-                Console.WriteLine($"Undoing checkout of {axlePart.Name}...")
+
+                Console.WriteLine($"Undoing check out of {axlePart.Name}....")
                 axlePart.UndoLockFile(handle, True)
                 Console.WriteLine($"Undo checkout of {axlePart.Name} = {Not axlePart.IsLocked}")
             Else
-                'check check in 
-                Console.WriteLine($"Checking in {axlePart.Name}...")
-                axlePart.UnlockFile(handle, "Checking file back into the vault.", handle)
+
+                Console.WriteLine($"Checking {axlePart.Name} back into the vault....")
+                axlePart.UnlockFile(handle, "Checking file back into the vault", 0)
                 Console.WriteLine($"Checked in {axlePart.Name} = {Not axlePart.IsLocked}")
-
             End If
-
         Else
-                ' print who has the file checked out 
-                Dim userId = axlePart.LockedByUserID
+            ' print who has the file checked out 
+            Dim userId As Integer
+            userId = axlePart.LockedByUserID
             Dim user = vault.GetObject(EdmObjectType.EdmObject_User, userId)
-            Console.WriteLine($"file is checked by {user.Name}")
+            Console.WriteLine($"file is checked out by {user.Name}")
+
         End If
 
-
+        Console.ReadLine()
 
     End Sub
+
 #Region "06"
-
-    Public Function CheckIn(ByVal file As IEdmFile5, Optional comment As String = "", Optional handle As Integer = 0) As Boolean
-        If (file IsNot Nothing) Then
-            file.UnlockFile(handle, comment, handle)
-            Return IsCheckedIn(file)
-        End If
-        Return False
-    End Function
-
-    Public Function Checkout(ByVal file As IEdmFile5, folderID As Integer, Optional handle As Integer = 0) As Boolean
-        If (file IsNot Nothing) Then
-            file.LockFile(handle, folderID)
-            Return IsCheckedOut(file)
-        End If
-        Return False
-    End Function
-
 
     Public Function IsCheckedOut(ByVal file As IEdmFile5) As Boolean
         If (file IsNot Nothing) Then
@@ -123,97 +114,108 @@ Module Program
         End If
         Return False
     End Function
-
     Public Function IsCheckedIn(ByVal file As IEdmFile5) As Boolean
         If (file IsNot Nothing) Then
             Return Not file.IsLocked
         End If
         Return False
     End Function
+    Public Function CheckIn(ByVal file As IEdmFile5, Optional comment As String = "", Optional handle As Integer = 0) As Boolean
+        If (file IsNot Nothing) Then
+            file.UnlockFile(handle, comment, 0)
+            Return IsCheckedIn(file)
+        End If
+        Return False
+    End Function
+    Public Function CheckOut(ByVal file As IEdmFile5, ByVal folder As IEdmFolder5, Optional handle As Integer = 0) As Boolean
+        If (file IsNot Nothing) Then
+            file.LockFile(handle, folder.ID)
+            Return IsCheckedOut(file)
+        End If
+        Return False
+    End Function
+
+
+
 
 #End Region
 
 #Region "05"
+
+
     Public Function GetAllFiles(ByVal folder As IEdmFolder5) As IEdmFile5()
 
         Dim files As New List(Of IEdmFile5)
 
-        Dim Position = folder.GetFirstFilePosition
-        While Not Position.IsNull
-            Dim iteratingFile = folder.GetNextFile(Position)
+        Dim position = folder.GetFirstFilePosition
+
+        While (Not position.IsNull)
+
+            Dim iteratingFile As IEdmFile5
+            iteratingFile = folder.GetNextFile(position)
             files.Add(iteratingFile)
         End While
-
         Return files.ToArray()
-
     End Function
 
-    Public Function GetAllSubFolders(ByVal folder As IEdmFolder5) As IEdmFolder5()
-
-        Dim folders As New List(Of IEdmFolder5)
-        Dim Position = folder.GetFirstSubFolderPosition
-        While Not Position.IsNull
-            Dim iteratingSubFolder = folder.GetNextSubFolder(Position)
-            folders.AddRange(GetAllFiles(iteratingSubFolder))
-        End While
-
-        Return folders.ToArray()
-
-    End Function
-
-    Public Function GetAllFiles(ByVal folder As IEdmFolder5, ByVal recursive As Boolean) As IEdmFile5()
+    Public Function GetAllFiles(ByVal folder As IEdmFolder5, ByVal GetAllSubFolders As Boolean) As IEdmFile5()
 
         Dim files As New List(Of IEdmFile5)
-        If (recursive = False) Then
+
+        If GetAllSubFolders = False Then
             Return GetAllFiles(folder)
         Else
 
             files.AddRange(GetAllFiles(folder))
 
-            Dim Position = folder.GetFirstSubFolderPosition
+            Dim position = folder.GetFirstSubFolderPosition
 
-            While Not Position.IsNull
-                Dim iteratingSubFolder = folder.GetNextSubFolder(Position)
-
-                files.AddRange(GetAllFiles(iteratingSubFolder))
-                Dim iteratingSubFolderFiles = GetAllFiles(iteratingSubFolder, True)
-                files.AddRange(iteratingSubFolderFiles)
-
+            While (Not position.IsNull)
+                Dim iteratingSubFolder As IEdmFolder5
+                iteratingSubFolder = folder.GetNextSubFolder(position)
+                files.AddRange(GetAllFiles(iteratingSubFolder, True))
             End While
 
+            Return files.ToArray()
+
         End If
-        Return files.ToArray()
     End Function
-    Public Function GetAllFiles(ByVal folder As IEdmFolder5, ByVal recursive As Boolean, ByRef CompletionResult As GetAllResult_e, Optional ByVal CancellationRequest As ICancellationRequest = Nothing) As IEdmFile5()
+    Public Function GetAllFiles(ByVal folder As IEdmFolder5, ByVal GetAllSubFolders As Boolean, ByRef completionState As CompletionState_e, Optional cancellationRequest As ICancellationRequest = Nothing) As IEdmFile5()
+
         Dim files As New List(Of IEdmFile5)
-        If (recursive = False) Then
+
+        If GetAllSubFolders = False Then
             Return GetAllFiles(folder)
         Else
+
             files.AddRange(GetAllFiles(folder))
 
             Try
-                Dim Position = folder.GetFirstSubFolderPosition
-                While Not Position.IsNull
-                    Dim iteratingSubFolder = folder.GetNextSubFolder(Position)
-                    files.AddRange(GetAllFiles(iteratingSubFolder))
-                    'check if user has requested cancellation 
-                    If (CancellationRequest IsNot Nothing) Then
-                        If CancellationRequest.IsCancellationRequested Then
-                            Throw New CancellationRequestThrownException()
+                Dim position = folder.GetFirstSubFolderPosition
+                While (Not position.IsNull)
+                    Dim iteratingSubFolder As IEdmFolder5
+                    iteratingSubFolder = folder.GetNextSubFolder(position)
+                    System.Threading.Thread.Sleep(1000)
+                    If cancellationRequest IsNot Nothing Then
+                        If cancellationRequest.IsCancellationRequestThrown Then
+                            Throw New CancellationRequestThrownException
                         End If
                     End If
-                    Dim iteratingSubFolderFiles = GetAllFiles(iteratingSubFolder, True)
-                    files.AddRange(iteratingSubFolderFiles)
+                    files.AddRange(GetAllFiles(iteratingSubFolder, True, completionState, cancellationRequest))
                 End While
             Catch ex As CancellationRequestThrownException
-                CompletionResult = GetAllResult_e.CancelledByUser
+                completionState = CompletionState_e.CancelledByUser
                 Return files.ToArray()
             End Try
+            completionState = CompletionState_e.CompletedSuccessfully
+            Return files.ToArray()
 
         End If
-        CompletionResult = GetAllResult_e.Completed
-        Return files.ToArray()
     End Function
+#End Region
+
+#Region "04"
+    'no helper sub or functions here
 #End Region
 
 #Region "03"
@@ -270,39 +272,36 @@ Module Program
 End Module
 
 #Region "05- cancellation classes "
-
-Public Enum GetAllResult_e
-    Completed
+Public Enum CompletionState_e
     CancelledByUser
+    CompletedSuccessfully
 End Enum
-Public Interface ICancellationRequest
-
-    Sub Cancel()
-    Property IsCancellationRequested() As Boolean
-
-End Interface
-
 Public Class CancellationRequestThrownException
     Inherits Exception
-
-
 End Class
+
 Public Class CancellationRequest
     Implements ICancellationRequest
 
-    Dim cancelled As Boolean = False
-    Public Property IsCancellationRequested As Boolean Implements ICancellationRequest.IsCancellationRequested
+    Private Cancelled As Boolean = False
+    Public Property IsCancellationRequestThrown As Boolean Implements ICancellationRequest.IsCancellationRequestThrown
         Get
-            Return cancelled
+            Return Cancelled
         End Get
-        Private Set(value As Boolean)
-            cancelled = value
+        Set(value As Boolean)
+            Cancelled = value
         End Set
     End Property
 
     Public Sub Cancel() Implements ICancellationRequest.Cancel
-        IsCancellationRequested = True
+        IsCancellationRequestThrown = True
     End Sub
 End Class
+
+Public Interface ICancellationRequest
+    Sub Cancel()
+    Property IsCancellationRequestThrown() As Boolean
+End Interface
+
 
 #End Region
