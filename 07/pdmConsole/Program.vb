0a7a3794af4891c6b0e856003629998cc4de7c74
jliliamen <amen@bluebyte.biz>
2020-06-09 21:49:34 -0700

    07

--- 07/pdmConsole/Program.vb
+++ 07/pdmConsole/Program.vb
@@ -1,4 +1,5 @@
-﻿Imports EPDM.Interop.epdm
+﻿Imports System.Runtime.InteropServices
+Imports EPDM.Interop.epdm
 Module Program
 
     Dim vault As IEdmVault5
@@ -44,141 +45,283 @@ Module Program
         End If
 
 
-        Dim folder As IEdmFolder5
-        Dim filePath As String = "C:\PDM2020\Example\Axle_&.sldprt"
-        Dim axlePart = vault.GetFileFromPath(filePath, folder)
+        Dim rootFolder As IEdmFolder5
+        rootFolder = vault.RootFolder
+        Dim exampleSubFolder As IEdmFolder5
 
-        Dim handle As Int32
+        exampleSubFolder = rootFolder.GetSubFolder("Example")
+
+        Dim filePath As String
+        filePath = "C:\PDM2020\Example\axle_&.sldprt"
+
+        Dim folder As IEdmFolder5
+        Dim axlePart As IEdmFile5
+
+        axlePart = vault.GetFileFromPath(filePath, folder)
+
+        Dim handle As Integer
         handle = System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle.ToInt32()
 
-        ' explanation:
-        ' Locked = False -> file Is checked In 
-        ' Locked = True  -> file Is checked out
+        'get vault variable mgr  
+        Dim variableMgr As IEdmVariableMgr5
+        variableMgr = vault
+        ' or get with the CreateUtility
 
-        If (axlePart.IsLocked = False) Then
-
-            'check file out 
-            Console.WriteLine($"Checking out {axlePart.Name}...")
-            axlePart.LockFile(folder.ID, handle, 0)
-            Console.WriteLine($"Checked out {axlePart.Name} = {axlePart.IsLocked}")
+        Dim variableMgr6 As IEdmVariableMgr6 = variableMgr
+        Dim configurationName As String = "@"
+        Dim variableEnumerator As IEdmEnumeratorVariable5
+        variableEnumerator = axlePart.GetEnumeratorVariable()
 
 
-            Console.WriteLine("Sleeping for several seconds...")
-            System.Threading.Thread.Sleep(3000)
 
-            Dim ret As String = String.Empty
+        'print all the variables names
+        Dim allVariableNames = GetAllVariables(variableMgr)
+        For Each variable In allVariableNames
+            Console.WriteLine(variable.Name)
+        Next
 
-            While (ret <> "y" And ret <> "n")
+        Dim variableName As String
+        variableName = "Description"
+        'set variable example on checked in file 
 
-                Console.WriteLine("Would you like to undo check out? [y] | [n]")
-                ret = Console.ReadLine()
-
-            End While
-
-            If (ret.Trim().ToLower() = "y") Then
-                'undo check out 
-                axlePart.UndoLockFile(handle, True)
-            Else
-                'check check out 
-                Console.WriteLine($"Checking in {axlePart.Name}...")
-                axlePart.UnlockFile(handle, "Checking file back into the vault.", 0)
-                Console.WriteLine($"Checked in {axlePart.Name} = {Not axlePart.IsLocked}")
-
-            End If
-
-        Else
-                ' print who has the file checked out 
-                Dim userId = axlePart.LockedByUserID
-            Dim user = vault.GetObject(EdmObjectType.EdmObject_User, userId)
-            Console.WriteLine($"file is checked by {user.Name}")
+        Dim variableSetReturn = SetDataVariableValue(axlePart, variableName, "Some random description", "@")
+        If (variableSetReturn.Item1 = False) Then
+            Console.WriteLine($"Something went wrong. {variableSetReturn.Item2}")
         End If
 
+        'check axle part out - this also gets the latest version  
+        CheckOut(axlePart, folder, handle)
 
+        'set the datacard variable description
+        Dim version As Integer = axlePart.CurrentVersion + 1
+        Dim variableValue As String = $"Version is : {version}"
+        'check axle part back into the vault 
+        variableSetReturn = SetDataVariableValue(axlePart, variableName, variableValue, "@")
+        If (variableSetReturn.Item1) Then
+            Console.WriteLine($"Sucessfully set Description to {variableValue}")
+        End If
+
+        'check axlePart back in 
+        CheckIn(axlePart, $"Set data card variable", handle)
+
+        'check part again and increase variable 
+        CheckOut(axlePart, folder, handle)
+
+        'set the datacard variable description
+        variableValue = $"Version is : {version + 1 }"
+        'check axle part back into the vault 
+        variableSetReturn = SetDataVariableValue(axlePart, variableName, variableValue, "@")
+        If (variableSetReturn.Item1) Then
+            Console.WriteLine($"Sucessfully set Description to {variableValue}")
+        End If
+
+        'check axlePart back in 
+        CheckIn(axlePart, $"Set data card variable", handle)
+
+
+        'get value of data card variable using GetVarFromDb
+        Dim Value = GetVariableValueFromDb(axlePart, folder.ID, variableName, "@")
+        Console.WriteLine($"Value using (GetVarFromDb): {Value}")
+        'get value of data card variable using GetVar
+        Value = GetDataCardVariableValue(axlePart, folder.ID, variableName, "@")
+        Console.WriteLine($"Value using (GetVar): {Value}")
+
+        'get version - 1 version of axle part 
+        axlePart.GetFileCopy(handle, version - 1, folder.ID)
+
+        'get value of data card variable using GetVarFromDb
+        Value = GetVariableValueFromDb(axlePart, folder.ID, variableName, "@")
+        Console.WriteLine($"Value using (GetVarFromDb): {Value}")
+        'get value of data card variable using GetVar
+        Value = GetDataCardVariableValue(axlePart, folder.ID, variableName, "@")
+        Console.WriteLine($"Value using (GetVar): {Value}")
+
+
+        'create variable free-version 
+        Dim variableMgr7 As IEdmVariableMgr7 = variableMgr
+        Dim edmVariableData As EdmVariableData
+        edmVariableData.mbsVariableName = "VersionFreeVariable"
+        edmVariableData.mlEdmVariableFlags = EdmVariableFlags.EdmVar_VerFreeUpdateAll
+        'add variable 
+        variableMgr7.AddVariables(New EdmVariableData() {edmVariableData})
+
+        'print variable 
+        allVariableNames = GetAllVariables(variableMgr)
+        For Each variable In allVariableNames
+            If (variable.Flags = EdmVariableFlags.EdmVar_VerFreeUpdateAll) Then
+                Console.WriteLine(variable.Name)
+            End If
+        Next
+
+        'edit 
+        variableMgr7.EditVariables(handle)
+
+
+        Console.ReadLine()
 
     End Sub
 
+#Region "07"
+
+
+
+    Public Function GetVariableValueFromDb(ByVal file As IEdmFile5, ByVal folderID As Integer, ByVal variableName As String, Optional ByVal configurationName As String = "@") As Object
+        Try
+            Dim value As Object
+            Dim variableEnumerator As IEdmEnumeratorVariable8
+            variableEnumerator = file.GetEnumeratorVariable()
+            variableEnumerator.GetVarFromDb(variableName, configurationName, value)
+            Return value
+        Catch ex As Exception
+            Return Nothing
+        End Try
+    End Function
+    Public Function GetDataCardVariableValue(ByVal file As IEdmFile5, ByVal folderID As Integer, ByVal variableName As String, Optional ByVal configurationName As String = "@") As Object
+        Try
+            Dim value As Object
+            Dim variableEnumerator As IEdmEnumeratorVariable8
+            variableEnumerator = file.GetEnumeratorVariable()
+            variableEnumerator.GetVar2(variableName, configurationName, value)
+            Return value
+        Catch ex As Exception
+            Return Nothing
+        End Try
+    End Function
+
+    Public Function SetDataVariableValue(ByVal file As IEdmFile5, ByVal variableName As String, ByVal value As Object, Optional ByVal configurationName As String = "@") As Tuple(Of Boolean, String, Object)
+        Try
+            Dim variableEnumerator As IEdmEnumeratorVariable8
+            variableEnumerator = file.GetEnumeratorVariable()
+            variableEnumerator.SetVar(variableName, configurationName, value, False)
+            variableEnumerator.CloseFile(True)
+            Return New Tuple(Of Boolean, String, Object)(True, String.Empty, value)
+        Catch ex As COMException
+            Return New Tuple(Of Boolean, String, Object)(False, ex.Message, Nothing)
+        End Try
+    End Function
+    Public Function GetAllVariables(ByVal variableMgr As IEdmVariableMgr5) As IEdmVariable5()
+        Dim variables As New List(Of IEdmVariable5)
+        Dim position = variableMgr.GetFirstVariablePosition
+        While Not position.IsNull
+            Dim iteratingVariable = variableMgr.GetNextVariable(position)
+            variables.Add(iteratingVariable)
+        End While
+        Return variables.ToArray()
+    End Function
+
+#End Region
+
+
+#Region "06"
+
+    Public Function IsCheckedOut(ByVal file As IEdmFile5) As Boolean
+        If (file IsNot Nothing) Then
+            Return file.IsLocked
+        End If
+        Return False
+    End Function
+    Public Function IsCheckedIn(ByVal file As IEdmFile5) As Boolean
+        If (file IsNot Nothing) Then
+            Return Not file.IsLocked
+        End If
+        Return False
+    End Function
+    Public Function CheckIn(ByVal file As IEdmFile5, Optional comment As String = "", Optional handle As Integer = 0) As Boolean
+        If (file IsNot Nothing) Then
+            file.UnlockFile(handle, comment, 0)
+            Return IsCheckedIn(file)
+        End If
+        Return False
+    End Function
+    Public Function CheckOut(ByVal file As IEdmFile5, ByVal folder As IEdmFolder5, Optional handle As Integer = 0) As Boolean
+        If (file IsNot Nothing) Then
+            file.LockFile(handle, folder.ID)
+            Return IsCheckedOut(file)
+        End If
+        Return False
+    End Function
+
+
+
+
+#End Region
 
 #Region "05"
+
+
     Public Function GetAllFiles(ByVal folder As IEdmFolder5) As IEdmFile5()
 
         Dim files As New List(Of IEdmFile5)
 
-        Dim Position = folder.GetFirstFilePosition
-        While Not Position.IsNull
-            Dim iteratingFile = folder.GetNextFile(Position)
+        Dim position = folder.GetFirstFilePosition
+
+        While (Not position.IsNull)
+
+            Dim iteratingFile As IEdmFile5
+            iteratingFile = folder.GetNextFile(position)
             files.Add(iteratingFile)
         End While
-
         Return files.ToArray()
-
     End Function
 
-    Public Function GetAllSubFolders(ByVal folder As IEdmFolder5) As IEdmFolder5()
-
-        Dim folders As New List(Of IEdmFolder5)
-        Dim Position = folder.GetFirstSubFolderPosition
-        While Not Position.IsNull
-            Dim iteratingSubFolder = folder.GetNextSubFolder(Position)
-            folders.AddRange(GetAllFiles(iteratingSubFolder))
-        End While
-
-        Return folders.ToArray()
-
-    End Function
-
-    Public Function GetAllFiles(ByVal folder As IEdmFolder5, ByVal recursive As Boolean) As IEdmFile5()
+    Public Function GetAllFiles(ByVal folder As IEdmFolder5, ByVal GetAllSubFolders As Boolean) As IEdmFile5()
 
         Dim files As New List(Of IEdmFile5)
-        If (recursive = False) Then
+
+        If GetAllSubFolders = False Then
             Return GetAllFiles(folder)
         Else
 
             files.AddRange(GetAllFiles(folder))
 
-            Dim Position = folder.GetFirstSubFolderPosition
+            Dim position = folder.GetFirstSubFolderPosition
 
-            While Not Position.IsNull
-                Dim iteratingSubFolder = folder.GetNextSubFolder(Position)
-
-                files.AddRange(GetAllFiles(iteratingSubFolder))
-                Dim iteratingSubFolderFiles = GetAllFiles(iteratingSubFolder, True)
-                files.AddRange(iteratingSubFolderFiles)
-
+            While (Not position.IsNull)
+                Dim iteratingSubFolder As IEdmFolder5
+                iteratingSubFolder = folder.GetNextSubFolder(position)
+                files.AddRange(GetAllFiles(iteratingSubFolder, True))
             End While
 
+            Return files.ToArray()
+
         End If
-        Return files.ToArray()
     End Function
-    Public Function GetAllFiles(ByVal folder As IEdmFolder5, ByVal recursive As Boolean, ByRef CompletionResult As GetAllResult_e, Optional ByVal CancellationRequest As ICancellationRequest = Nothing) As IEdmFile5()
+    Public Function GetAllFiles(ByVal folder As IEdmFolder5, ByVal GetAllSubFolders As Boolean, ByRef completionState As CompletionState_e, Optional cancellationRequest As ICancellationRequest = Nothing) As IEdmFile5()
+
         Dim files As New List(Of IEdmFile5)
-        If (recursive = False) Then
+
+        If GetAllSubFolders = False Then
             Return GetAllFiles(folder)
         Else
+
             files.AddRange(GetAllFiles(folder))
 
             Try
-                Dim Position = folder.GetFirstSubFolderPosition
-                While Not Position.IsNull
-                    Dim iteratingSubFolder = folder.GetNextSubFolder(Position)
-                    files.AddRange(GetAllFiles(iteratingSubFolder))
-                    'check if user has requested cancellation 
-                    If (CancellationRequest IsNot Nothing) Then
-                        If CancellationRequest.IsCancellationRequested Then
-                            Throw New CancellationRequestThrownException()
+                Dim position = folder.GetFirstSubFolderPosition
+                While (Not position.IsNull)
+                    Dim iteratingSubFolder As IEdmFolder5
+                    iteratingSubFolder = folder.GetNextSubFolder(position)
+                    System.Threading.Thread.Sleep(1000)
+                    If cancellationRequest IsNot Nothing Then
+                        If cancellationRequest.IsCancellationRequestThrown Then
+                            Throw New CancellationRequestThrownException
                         End If
                     End If
-                    Dim iteratingSubFolderFiles = GetAllFiles(iteratingSubFolder, True)
-                    files.AddRange(iteratingSubFolderFiles)
+                    files.AddRange(GetAllFiles(iteratingSubFolder, True, completionState, cancellationRequest))
                 End While
             Catch ex As CancellationRequestThrownException
-                CompletionResult = GetAllResult_e.CancelledByUser
+                completionState = CompletionState_e.CancelledByUser
                 Return files.ToArray()
             End Try
+            completionState = CompletionState_e.CompletedSuccessfully
+            Return files.ToArray()
 
         End If
-        CompletionResult = GetAllResult_e.Completed
-        Return files.ToArray()
     End Function
+#End Region
+
+#Region "04"
+    'no helper sub or functions here
 #End Region
 
 #Region "03"
@@ -235,39 +378,36 @@ Module Program
 End Module
 
 #Region "05- cancellation classes "
-
-Public Enum GetAllResult_e
-    Completed
+Public Enum CompletionState_e
     CancelledByUser
+    CompletedSuccessfully
 End Enum
-Public Interface ICancellationRequest
-
-    Sub Cancel()
-    Property IsCancellationRequested() As Boolean
-
-End Interface
-
 Public Class CancellationRequestThrownException
     Inherits Exception
-
-
 End Class
+
 Public Class CancellationRequest
     Implements ICancellationRequest
 
-    Dim cancelled As Boolean = False
-    Public Property IsCancellationRequested As Boolean Implements ICancellationRequest.IsCancellationRequested
+    Private Cancelled As Boolean = False
+    Public Property IsCancellationRequestThrown As Boolean Implements ICancellationRequest.IsCancellationRequestThrown
         Get
-            Return cancelled
+            Return Cancelled
         End Get
-        Private Set(value As Boolean)
-            cancelled = value
+        Set(value As Boolean)
+            Cancelled = value
         End Set
     End Property
 
     Public Sub Cancel() Implements ICancellationRequest.Cancel
-        IsCancellationRequested = True
+        IsCancellationRequestThrown = True
     End Sub
 End Class
+
+Public Interface ICancellationRequest
+    Sub Cancel()
+    Property IsCancellationRequestThrown() As Boolean
+End Interface
+
 
 #End Region
